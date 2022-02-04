cfc9e20fc5016dc8192071128b6ed5ae4915f352
namsewon1019 <swnam0729@gmail.com>
2022-02-04 13:33:52 +0900

    2022-02-04 queryMethod 정의와 실습

--- bookmanager/src/main/java/com/example/bookmanager/repository/UserRepository.java
+++ bookmanager/src/main/java/com/example/bookmanager/repository/UserRepository.java
@@ -3,8 +3,35 @@ package com.example.bookmanager.repository;
 import com.example.bookmanager.domain.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface UserRepository extends JpaRepository<User, Long> {
 
     //JpaRepository 를 상속받음으로써 jpa의 함수(ex : findAll())들을 사용할 수 있다.
     // <> 안에 첫번째로 Entity의 type, 두번째는 PK값의 type
+
+    User findByName(String name); // name을 통해서 User을 가져오는 method를 만듬.
+
+    User findByEmail(String email);
+
+    User getByEmail(String email);
+
+    User readByEmail(String email);
+
+    User queryByEmail(String email);
+
+    User searchByEmail(String email);
+
+    User streamByEmail(String email);
+
+    User findUserByEmail(String email);
+
+    User findSomethingByEmail(String email); // entity 자리에 something이 들어와도 정상적으로 작동함
+
+    List<User> findTop1ByName(String name);
+    List<User> findFirst1ByName(String name);
+
+    List<User> findLast1ByName(String name);
+
+
 }
--- bookmanager/src/test/java/com/example/bookmanager/repository/UserRepositoryTest.java
+++ bookmanager/src/test/java/com/example/bookmanager/repository/UserRepositoryTest.java
@@ -19,4 +19,24 @@ class UserRepositoryTest {
         //System.out.println(user.toString());
 
     }
+
+    @Test
+    void select() {
+        System.out.println(userRepository.findByName("steve"));
+
+        System.out.println("findByEmail :  " + userRepository.findByEmail("steve"));
+        System.out.println("getByEmail :  " + userRepository.getByEmail("steve"));
+        System.out.println("readByEmail :  " + userRepository.readByEmail("steve"));
+        System.out.println("queryByEmail :  " + userRepository.queryByEmail("steve"));
+        System.out.println("searchByEmail :  " + userRepository.searchByEmail("steve"));
+        System.out.println("streamByEmail :  " + userRepository.streamByEmail("steve"));
+        System.out.println("findUserByEmail :  " + userRepository.findUserByEmail("steve"));
+
+        System.out.println("findSomethingByEmail : " + userRepository.findSomethingByEmail("steve@gamil.com"));
+
+        System.out.println("findFirst1ByName : " + userRepository.findFirst1ByName("steve"));
+        System.out.println("findTop1ByName : " + userRepository.findTop1ByName("steve"));
+
+        System.out.println("findLast1ByName : " + userRepository.findLast1ByName("steve"));
+    }
 }
